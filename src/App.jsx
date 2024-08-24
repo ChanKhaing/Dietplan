6f8d4f4b1bb9c5b8ef8d9d7a8da7f3384a5f93ae
Sitnyein <chankhinem@gmail.com>
2024-08-25 00:02:04 +0630

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Navbar from './components/Navbar'
 import Coversection from './components/Coversection'
 import HeroSection from './components/HeroSection'
 import ProdutService from './components/ProdutService'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
       <div>
       <HeroSection />
       <ProdutService />
+      <Footer />
       </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,29 +1,27 @@
 import React from 'react'
+import Product1 from '../assets/image/product1.jpg'
 
 const Card = () => {
   return (
     <div>
-      <div className=" bg-black text-white w-72 h-96 rounded-lg p-4">
-        <h1 className="text-3xl font-bold text-center">Special Set</h1>
-        <p className="text-lg text-center">
+      <div className=" bg-black text-white w-72 rounded-lg p-4 mt-44">
+        <div className="flex justify-center relative h-48 p-4">
+          <img
+            src={Product1}
+            alt="Special Set"
+            className=" w-44 h-48 object-cover rounded-full absolute -top-1/2 left-0"
+          />
+        </div>
+        <h1 className="text-2xl font-bold  ">Special Set</h1>
+        <p className="text-lg ">
           Energize with our nutritious healthy salad, bursting with fresh greens
           and vibrant superfoods.
         </p>
-        <div className="flex justify-center mt-4">
-          <img
-            src="/path/to/your/image.jpg"
-            alt="Special Set"
-            className="w-48 h-48 object-cover rounded-full"
-          />
-        </div>
-        <div className="flex justify-center mt-4">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4   
- rounded"
-          >
-            Add to Cart
-          </button>
-        </div>
+        <button
+          className=" text-black bg-white w-full font-bold py-2 px-4 rounded-lg"
+        >
+          Add to Cart
+        </button>
       </div>
     </div>
   );
