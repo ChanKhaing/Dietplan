3d86c86d31d4e1f4eae76ded3f2d80790ede9de6
Sitnyein <chankhinem@gmail.com>
2024-08-29 03:48:22 +0630

    produtpage

--- src/components/Contactmap.jsx
+++ src/components/Contactmap.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import { Container } from "./Container";
 
 const Contactmap = () => {
   return (
-    <div className=" mt-44">
-      <section className="  right-0 text-center mb-32 ">
+    <div className=" mt-44 mb-44">
+      <section className="  right-0 text-center mb-20 ">
         <span className=" inline-block right-0 text-center font-serif  ">
           ___Contact with us___
         </span>
@@ -12,8 +13,54 @@ const Contactmap = () => {
           Feel free to contact us for any assistance you may need
         </p>
       </section>
+
+      <Container>
+        <section className=" flex justify-between">
+          <div>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d282.2278004210445!2d96.13915756278593!3d16.806500384142712!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30c1edbb8a89416d%3A0x64420f52367032b9!2sMMS%20IT!5e1!3m2!1sen!2smy!4v1686286820285!5m2!1sen!2smy"
+              className=" w-[600px] h-[600px] rounded-2xl"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+
+          <div className=" w-[600px] h-[600px] rounded-2xl ">
+            <span className=" block mb-4 text-lg rounded-lg">Full Name</span>
+            <input
+              className="block w-full border disabled:cursor-not-allowed disabled:opacity-50 border-gray-300 bg-gray-50 text-gray-900   p-2.5 text-sm rounded-lg"
+              type="text"
+              required=""
+              id="Full Name"
+            ></input>
+
+            <span className=" block mb-4 text-lg mt-10 rounded-lg">Email</span>
+            <input
+              class="block w-full border disabled:cursor-not-allowed disabled:opacity-50 border-gray-300 bg-gray-50 text-gray-900   p-2.5 text-sm rounded-lg"
+              type="text"
+              required=""
+              id="Email"
+            ></input>
+
+            <span className=" block mb-4  text-lg mt-10 rounded-lg">
+              Message
+            </span>
+            <textarea
+              class="block w-full h-[200px] mb-4 border disabled:cursor-not-allowed disabled:opacity-50 border-gray-300 bg-gray-50 text-gray-900   p-2.5 text-sm rounded-lg"
+              type="text"
+              required=""
+              id="Quantity"
+            ></textarea>
+
+            <button class="  p-2  font-serif rounded-lg    items-center m-2 text-white bg-slate-800 border-0  px-8 focus:outline-none  text-lg">
+              SUBMIT REQUEST
+            </button>
+          </div>
+        </section>
+      </Container>
     </div>
   );
-}
+};
 
-export default Contactmap
+export default Contactmap;
--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -3,13 +3,6 @@ import { Container } from './Container';
 
 const FAQ = () => {
   return (
-    // <div>
-    //   <Container>
-    //     <p>__HelpfulInformation__</p>
-    //     <h1> Frequently Ask Questions </h1>
-    //     <h3>Explore our comprehensive for dieting and nutrition</h3>
-    //   </Container>
-    // </div>
     <div>
       <section className=" mt-44 right-0 text-center mb-32 ">
         <span className=" inline-block right-0 text-center font-serif  ">
@@ -25,42 +18,126 @@ const FAQ = () => {
         </p>
       </section>
 
-      <section className=" m-10 h-[500px] bg-slate-600">
+      {/* <main className=" mt-10 h-[400px]   bg-slate-600">
         <Container>
-          <div>
-            <section>
-              <div className=" mb-5">
+          <div className=" grid grid-cols-2 gap-5 p-6">
+            <section className="">
+              <div className=" my-5">
                 <p className=" font-bold text-xl block mb-5">
                   How does your diet plan service work?{" "}
                 </p>
                 <span className=" block  ">
-                                  Plans are updated monthly to ensure continued progress and
-                                  <br></br>
+                  Plans are updated monthly to ensure continued progress and
+                  <br></br>
                   cater to your evolving preferences
                 </span>
               </div>
-              <div className=" mb-5">
+              <div className=" mb-5 ">
                 <p className=" font-bold text-xl block">
-                  How does your diet plan service work?{" "}
+                  What is the cancellation policy?
                 </p>
-                <span className="">
-                  Plans are updated monthly to ensure continued progress and
-                  cater to your evolving preferences.
+                <span className=" block">
+                  You can cancel your subscription at any time. Please refer to
+                  our <br /> terms of service for details.
                 </span>
               </div>
-              <div className=" mb-5">
-                <p className=" font-bold text-xl block">
+            </section>
+
+            <section className="">
+              <div className=" my-5">
+                <p className=" font-bold text-xl block mb-5">
                   How does your diet plan service work?{" "}
                 </p>
-                <span className="">
+                <span className=" block  ">
                   Plans are updated monthly to ensure continued progress and
-                  cater to your evolving preferences.
+                  <br></br>
+                  cater to your evolving preferences
+                </span>
+              </div>
+              <div className=" mb-5 ">
+                <p className=" font-bold text-xl block">
+                  What is the cancellation policy?
+                </p>
+                <span className=" block">
+                  You can cancel your subscription at any time. Please refer to
+                  our <br /> terms of service for details.
                 </span>
               </div>
             </section>
           </div>
         </Container>
-      </section>
+      </main> */}
+
+      <Container>
+        <main className=" mt-10 h-[400px]  mx-auto w-[950px]  ">
+          <div className=" flex gap-4  ">
+            <section className="">
+              <div className=" my-5">
+                <p className=" font-bold text-xl block mb-5">
+                  How does your diet plan service work?{" "}
+                </p>
+                <span className=" block  text-gray-500 ">
+                  Plans are updated monthly to ensure continued progress and
+                  <br></br>
+                  cater to your evolving preferences
+                </span>
+              </div>
+              <div className=" mb-5 ">
+                <p className=" font-bold text-xl block mb-5">
+                  What is the cancellation policy?
+                </p>
+                <span className=" block  text-gray-500 ">
+                  You can cancel your subscription at any time. Please refer to
+                  our <br /> terms of service for details.
+                </span>
+              </div>
+
+              <div className=" mb-5 ">
+                <p className=" font-bold text-xl block mb-5">
+                  Will I receive support from a nutritionist?
+                </p>
+                <span className=" block text-gray-500">
+                  Yes, our plans come with access to certified nutritionists for
+                  <br /> guidance and support throughout your journey.
+                </span>
+              </div>
+            </section>
+
+            <section className="">
+              <div className=" my-5">
+                <p className=" font-bold text-xl block mb-5">
+                  How do I get started with a diet plan?(20){" "}
+                </p>
+                <span className=" block text-gray-500  ">
+                  Simply sign up on our website, provide your details, and our{" "}
+                  <br />
+                  team will create a tailored plan for you.
+                </span>
+              </div>
+              <div className=" mb-5 ">
+                <p className=" font-bold text-xl block mb-5">
+                  What type of diets do you offer?
+                </p>
+                <span className=" block text-gray-500">
+                  We provide a range of diet options including balanced, keto,{" "}
+                  <br />
+                  vegetarian, and more. You can choose based on your needs.
+                </span>
+              </div>
+
+              <div className=" mb-5 ">
+                <p className=" font-bold text-xl block mb-5">
+                  Can I customize my diet plan?
+                </p>
+                <span className=" block text-gray-500">
+                  Yes, you can customize your plan based on dietary
+                  restrictions, <br /> allergies, and food preferences.
+                </span>
+              </div>
+            </section>
+          </div>
+        </main>
+      </Container>
     </div>
   );
 }
