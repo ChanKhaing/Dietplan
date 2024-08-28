e0817b4d473bad1d9e68e315def1aae8d608b315
Sitnyein <chankhinem@gmail.com>
2024-08-28 19:46:30 +0630

    prepare faq

---
+++ src/components/Contactway.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Contactway = () => {
+  return (
+    <div className=" mt-44 right-0 text-center mb-32 ">
+      <span className=" inline-block right-0 text-center font-serif ">
+        {" "}
+        ___Contact Us___
+      </span>
+      <h1 className=" text-3xl  ">Get in touch with us</h1>
+      <p className=" right-0 text-center">
+        Contact for assistance and personalized dietary guidance
+          </p>
+          
+    </div>
+  );
+}
+
+export default Contactway
--- src/pages/Contactus.jsx
+++ src/pages/Contactus.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ContactCover from '../components/ContactCover'
+import Contactway from '../components/Contactway'
+import Contactbox from '../components/Contactbox'
 
 const Contactus = () => {
   return (
     <>
-    <ContactCover/>
+      <ContactCover />
+      <Contactway />
+      <Contactbox/>d
     </>
   )
 }
